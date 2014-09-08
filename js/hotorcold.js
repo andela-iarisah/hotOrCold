3aa45849475df3c481bb781a27ff4734b76bfb10
Ijeoma <gift_ij2002@yahoo.com>
2014-09-08 09:37:28 +0100

    initializing repo

--- js/hotorcold.js
+++ js/hotorcold.js
@@ -1,30 +1,38 @@
-	var cmpChoice = Math.ceil(Math.random() * 100);
-	$(document).ready(function() {
+$(document).ready(function() {
+	var cmpChoice = Math.ceil(Math.random() * 100);	
+		$('input#submit').click(function() {
+			function compare() {
+				var userChoice = document.getElementById('digit').value;
+				var userChoiceInt = parseInt(userChoice);
+				var newGuess = 0;
+					if (isNaN(userChoiceInt) || userChoiceInt > 100 || userChoiceInt < 0) {
+						$('p.status').append("Please put in a valid input");
+					}
+					else {
+						if (userChoiceInt === cmpChoice) {
+						$('p.status').append("<p>I give up! You are a darn good guesser</p>");
+					}
+						// else {
+						// 	function reCheck = 
+						// }
+				
 
-	
-	var prevGuess = 0;
-	var compare = function() {
-	$('#submit').click(function() {
-		var userChoice = document.getElementById('digit').value;
-		var userChoiceInt = parseInt(userChoice);
-			if(isNaN(userChoiceInt) || userChoiceInt > 100 || userChoiceInt < 0) {
-				alert('Please enter a valid input');
-			}
-			else {
-				if (userChoiceInt === cmpChoice) {
-				alert("I give up! You're a darn good guesser");
-			}
-			else if (userChoiceInt < cmpChoice) {
-				alert("You're cold");
-			} 
-			else {
-				alert("You're hot");
-			}
-		}
-	});
-		
-};
-	compare();
-	console.log(cmpChoice);
 
-});	
+
+				// 		else if (userChoiceInt < cmpChoice) {
+				// 		$('p.status').append("<p>You are cold</p>");
+				// 	} 
+				// 		else {
+				// 		$('p.status').append("<p>You are hot</p>");
+				// 	}
+				// }
+				// 	$('p.status').
+					// $('input#digit').setAttribute(userChoiceInt, newGuess) 
+					// if ((cmpChoice - newGuess) > (cmpChoice - oldGuess)) {
+					// 	$('div.guage').append("<p>You are hotter</p>");
+					// }
+	};
+			compare();
+			console.log(cmpChoice);
+});
+});
